56eb165989772a4bee141caf289c7ef5b6ed68e0
hadrihl <hadrihilmi@gmail.com>
2024-03-03 20:54:09 +0800

    initial commit.

---
+++ src/main/java/com/example/kais/KaisApplication.java
@@ -0,0 +1,13 @@
+package com.example.kais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kais/KaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
