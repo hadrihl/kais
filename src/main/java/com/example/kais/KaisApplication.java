940f561be18a31b3ec6a0646d1778b58c4833361
hadrihl <hadrihilmi@gmail.com>
2024-03-03 21:13:04 +0800

    config to deploy app as WAR in servlet container.

--- src/main/java/com/example/kais/KaisApplication.java
+++ src/main/java/com/example/kais/KaisApplication.java
@@ -2,9 +2,15 @@ package com.example.kais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KaisApplication {
+public class KaisApplication extends SpringBootServletInitializer {
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KaisApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KaisApplication.class, args);
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=US-ASCII"
+    pageEncoding="US-ASCII"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="US-ASCII">
+<title>KAIS | Homepage</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Welcome to KAIS system</h1>
+		
+		<a href="/register">Register</a>
+		<a href="/login">Login</a>
+	</div>
+	<footer style="margin-top: 20px;"><a href="/">KAIS</a> &copy; 2024. Made with love in Machang.</footer>
+</body>
+</html>
