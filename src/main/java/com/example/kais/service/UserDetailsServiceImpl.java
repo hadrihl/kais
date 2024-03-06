24c21b770ad8056184d6ebaef5b48df295241621
hadrihl <hadrihl@outlook.com>
2024-03-06 09:18:50 +0800

    Added login feature.

---
+++ src/main/java/com/example/kais/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.kais.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.example.kais.entity.User;
+import com.example.kais.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.getUserByUsername(username);
+		
+		if(user == null) {
+			throw new UsernameNotFoundException("Username not found.");
+		}
+		
+		return new CustomUserDetails(user);
+	}
+}
